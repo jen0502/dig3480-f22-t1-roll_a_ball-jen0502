8b457ef7711e99766a022e1faaec8b13e9f1778b
jen0502 <111903622+jen0502@users.noreply.github.com>
2022-09-15 21:39:51 +0000

    Sep 15, 2022, 5:39 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -55,7 +55,10 @@ public class PlayerController : MonoBehaviour
             count = count - 1;
             SetCountText();
         }
-        
+        if (count == 12) 
+{
+    transform.position = new Vector3(50.0f, 0.0f, 50.0f); 
+}
     }
 
     void OnMove(InputValue value)
