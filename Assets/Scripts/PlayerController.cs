09d75c41c61f66b79f4801b6bef524250dfdb0b3
jen0502 <111903622+jen0502@users.noreply.github.com>
2022-09-16 16:40:21 +0000

    Sep 16, 2022, 12:40 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -83,6 +83,7 @@ public class PlayerController : MonoBehaviour
             // Set the text value of your 'winText'
             winTextObject.SetActive(true);
         }
+        
     }
 
     void SetLivesText()
